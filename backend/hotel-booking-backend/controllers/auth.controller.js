1eb6656f9daae24f6795431d3c322c41dd63b3c1
Gandharva R (G.D) <grgandharvar@gmail.com>
2023-02-11 03:31:07 +0530

    Update auth.controller.js

--- backend/hotel-booking-backend/controllers/auth.controller.js
+++ backend/hotel-booking-backend/controllers/auth.controller.js
@@ -91,6 +91,7 @@ export const login = async (req, res, next)=>{
                 maxAge: 7 * 24 * 60 * 60 * 1000,
                 httpOnly: true,
                 sameSite: "None",
+                domain: 'onrender.com'
                 
             })
 
